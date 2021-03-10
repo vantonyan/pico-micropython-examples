a906c76a4bf63dcf72e9d715113ae2f3d3a182a4
vantonyan <tony_912@yahoo.com>
2021-03-10 08:49:10 -0800

    Update pwm_fade.py
    This change will make it more visible and continuous display. Original code runs so quickly that after loading I missed the LED blinking

--- pwm/pwm_fade.py
+++ pwm/pwm_fade.py
@@ -13,13 +13,14 @@ pwm.freq(1000)
 # Fade the LED in and out a few times.
 duty = 0
 direction = 1
-for _ in range(8 * 256):
-    duty += direction
-    if duty > 255:
-        duty = 255
-        direction = -1
-    elif duty < 0:
-        duty = 0
-        direction = 1
-    pwm.duty_u16(duty * duty)
-    time.sleep(0.001)
+while True:
+    for _ in range(8 * 256):
+        duty += direction
+        if duty > 255:
+            duty = 255
+            direction = -1
+        elif duty < 0:
+            duty = 0
+            direction = 1
+        pwm.duty_u16(duty * duty)
+        time.sleep(0.005)
